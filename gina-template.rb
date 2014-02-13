7e955071c32e5d7398f64df418ef680c12b5fa82
Will Fisher <will@teknofire.net>
2014-02-13 11:35:11 -0900

    fixing small issue with bootstrap and font-awesome includes

--- gina-template.rb
+++ gina-template.rb
@@ -16,11 +16,11 @@ run "bower install bootstrap"
 run "bower install font-awesome"
 
 inject_into_file("app/assets/javascripts/application.js", after: "//= require jquery_ujs\n") do
-  "//= require bootstrap/dist/js/bootstrap"
+  "//= require bootstrap/dist/js/bootstrap\n"
 end
 inject_into_file("app/assets/stylesheets/application.css", before: " *= require_self\n") do
   " *= require bootstrap/dist/css/bootstrap
- *= require font-awesome/css/font-awesome"
+ *= require font-awesome/css/font-awesome\n"
 end
 
 rake "db:migrate"
